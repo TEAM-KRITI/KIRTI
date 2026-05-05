55a294c48519c126227254eb9d7c9dcb0c81c2a2
⎯᪵⎯꯭̽🚬᪵᪳ ⃪꯭𝐁ʌ‌꯭ᴅ꯭𝐍꯭ᴀ‌꯭ᴍ꯭ ꯭🖤᪵᪳᪳𝆺꯭𝅥⎯꯭̽⟶᯦꯭ <rejemtahir@gmail.com>
2026-05-05 12:31:41 +0530

    Update import paths for KIRTI_MUSIC module

--- KIRTI_MUSIC/utils/inline/play.py
+++ KIRTI_MUSIC/utils/inline/play.py
@@ -1,9 +1,9 @@
 import math
 from config import SUPPORT_CHAT, OWNER_USERNAME
 from pyrogram.types import InlineKeyboardButton
-from SONALI_MUSIC import app
+from KIRTI_MUSIC import app
 import config
-from SONALI_MUSIC.utils.formatters import time_to_seconds
+from KIRTI_MUSIC.utils.formatters import time_to_seconds
 
 
 def track_markup(_, videoid, user_id, channel, fplay):
